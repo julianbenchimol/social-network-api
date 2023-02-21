71943b49100614db4794746ff853eb20a749d003
Julian Benchimol <jbenchimol1999@gmail.com>
2023-02-21 10:53:53 -0500

    Updated User Controller

---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+const { ObjectId} = require ('mongoose').Types;
+const {User} = require('../models/User');
+
+
+module.exports = {
+    getUsers(req, res){
+        User.find()
+        .then(async (users) =>{
+            const userObject = {
+                users
+            }
+            return res.json(userObject)
+        })
+        .catch((err) =>{
+            console.log(err);
+            return res.status(500).json(err)
+        })
+    },
+    getOneUser(req, res){
+        User.findOne({_id: req.params.userId})
+        .select('-__v')
+        .then(async (user)=>{
+            !user
+            ?res.status(404).json({message: "No User Exists with this ID."})
+            : res.json({user})
+        })
+        .catch((err)=>{
+            console.log(err);
+            return res.status(500).json(err)
+        })
+
+    },
+    createNewUser(req, res){
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err)=> res.status(500).json(err))
+    },
+    updateUser(req, res){
+        User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$seet: req.body},
+            {runValidators: true, new: true}
+        )
+        .then((user) =>
+            !user  
+            ? res.status(404).json({message: "This User does not Exist"})
+            : res.json(user)
+        )
+        .catch((err)=> res.status(500).json(err))
+    }
+};
